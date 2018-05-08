c3300536099e98c1a9c075d6378ecd7c7aac0cc9
Maatuq <mahmoudmatook.mm@gmail.com>
2018-05-08 07:43:57 -0400

    ex2-5 image blurring

--- 0.c
+++ 0.c
@@ -1,9 +1,9 @@
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
 using namespace cv;
-int main( int argc, char** argv ) {
+    int main( int argc, char** argv ) {
 Mat img = imread( argv[1], -1 );
 if( img.empty() ) return -1;
-namedWindow( "Example2", WINDOW_AUTOSIZE );
+namedWindow( "Example2", 0);
 imshow( "Example2", img );
 waitKey( 0 );
 destroyWindow( "Example2" );
